5747752c7100e725cbde5a4a75638c7785b9f8ab
Prajwal pandey <Prajwalpandey2009@gmail.com>
2023-06-02 16:40:02 +0530

    added a simple html template and created a route for it

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ app.get('/route2', (req, res) =>{
     res.send('Hello world from route 2');
 })
 
+app.get('/chat', ( req, res) => {
+    res.send(
+        `<html>
+            <head>
+                <title>The simple html page</title>
+            </head>
+            <body>
+                <h1 style="color: blue">Chat</h1>
+            </body>
+        </html>`
+    )
+})
+
 app.listen(port, () =>{
     console.log(`http server is running on port ${port}`);
 })
